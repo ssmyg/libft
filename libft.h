36edff9451ef69ada5a8d59ed79da532b5eab4a9
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-18 17:07:23 +0900

    add isalnum, isalpha, isdigit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:49:00 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/18 16:25:18 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/18 17:00:21 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_atoi(char *str);
-void ft_bzero(void *s, size_t n);
+int		ft_atoi(char *str);
+void	ft_bzero(void *s, size_t n);
+//void	*calloc(size_t count, size_t size); // memset実装後に作成
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 #endif
