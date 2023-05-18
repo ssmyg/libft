2a97c48d391a8153adcee6a56af7485188b63caf
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-18 16:26:09 +0900

    add bzero

--- libft.h
+++ libft.h
@@ -6,13 +6,16 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:49:00 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/18 15:51:15 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/18 16:25:18 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
 int	ft_atoi(char *str);
+void ft_bzero(void *s, size_t n);
 
 #endif
