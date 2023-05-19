de6ba25aad4756b1682cd7e9f8e3398ee680b593
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-19 14:21:41 +0900

    add strchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:49:00 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/19 13:19:09 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/19 13:31:44 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
+
+char	*ft_strchr(const char *s, int c);
 
 #endif
