3c65eb5f123bb50ae4139bd47bc4367ecd44d18a
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-19 13:17:25 +0900

    refactoring memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 18:18:05 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/18 19:02:39 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/19 13:16:15 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,5 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		*(char *)(dst + i) = *(char *)(src + i);
-		i++;
-	}
-	return (dst);
+	return (ft_memmove(dst, src, n));
 }
