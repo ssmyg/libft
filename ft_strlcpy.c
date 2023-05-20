0d3d2bdabfd1d485e2aa6affe6cccc01186d9ed6
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-20 14:28:11 +0900

    clean strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 16:34:30 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/20 10:29:00 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/20 10:46:01 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 			*(dst + i) = *(src + i);
 		i++;
 	}
-	if (i > dstsize - 1)
+	if (i + 1 > dstsize)
 		*(dst + dstsize - 1) = '\0';
 	if (i < dstsize)
 		*(dst + i) = '\0';
