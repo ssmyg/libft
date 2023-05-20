e0e9cb45223f9ecb29ed3f7a9e16bea2fae63535
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-20 15:57:02 +0900

    add strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 14:30:58 by susumuyagi        #+#    #+#             */
+/*   Updated: 2023/05/20 15:49:52 by susumuyagi       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	concatenate(char *restrict dst, const char *restrict src,
+		size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(dst + i))
+		i++;
+	while (*src && i + 1 < dstsize)
+	{
+		*(dst + i) = *src;
+		i++;
+		src++;
+	}
+	if (i + 1 > dstsize)
+		*(dst + dstsize - 1) = '\0';
+	if (i < dstsize)
+		*(dst + i) = '\0';
+}
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	dst_len;
+
+	src_len = ft_strlen(src);
+	if (!dst)
+		return (src_len);
+	dst_len = ft_strlen(dst);
+	if (dstsize <= src_len && dstsize < dst_len)
+		return (src_len + dstsize);
+	if (dstsize <= dst_len)
+		return (src_len + dst_len);
+	concatenate(dst, src, dstsize);
+	return (src_len + dst_len);
+}
--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:49:00 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/19 13:31:44 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/19 16:35:33 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <stddef.h>
 
 int		ft_atoi(char *str);
 void	ft_bzero(void *s, size_t n);
@@ -32,5 +32,11 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 
 char	*ft_strchr(const char *s, int c);
+// char	*ft_strdup(const char *s1); // strlen実装後に作成
+size_t	ft_strlcat(char *restrict dst, const char *restrict src,
+			size_t dstsize);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
+			size_t dstsize);
+size_t	ft_strlen(const char *s);
 
 #endif
