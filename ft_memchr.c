8b3d596ba640f31a73b235bddc8f73d099823f3f
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-21 17:21:40 +0900

    norm memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 13:20:46 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/19 13:20:49 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/21 17:21:12 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (*(char *)(s + i) == (char)c)
-			return (void *)(s + i);
+			return ((void *)(s + i));
 		i++;
 	}
 	return (NULL);
