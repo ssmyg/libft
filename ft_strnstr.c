8ce8bcbed3934941c24b1a2be539f220883af4b8
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-21 17:20:58 +0900

    norm strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,22 +6,18 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:13:05 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/21 17:07:46 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/21 17:19:42 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+static char	*search(const char *haystack, const char *needle, size_t len)
 {
 	char	*ret;
 	char	*orig_needle;
 	size_t	i;
 
-	if (!haystack)
-		return (NULL);
-	if (*needle == '\0')
-		return ((char *)haystack);
 	ret = NULL;
 	orig_needle = (char *)needle;
 	i = 0;
@@ -41,4 +37,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		i++;
 	}
 	return (NULL);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	if (!haystack)
+		return (NULL);
+	if (*needle == '\0')
+		return ((char *)haystack);
+	return (search(haystack, needle, len));
 }
