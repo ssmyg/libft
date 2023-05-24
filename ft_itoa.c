ed553c4edf014eb4f8f0d8feb8486ef67c36dc63
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-25 08:57:25 +0900

    fix itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 11:42:07 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/23 12:12:26 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/25 08:55:16 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,38 @@ static size_t	count_len(int n)
 	return (count);
 }
 
+static void	set_nbr_str(char *str, int n, size_t len)
+{
+	int	sign;
+
+	sign = 1;
+	if (n < 0)
+	{
+		sign = -1;
+		str[0] = '-';
+	}
+	str[len] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	while (n)
+	{
+		str[len - 1] = '0' + sign * (n % 10);
+		n /= 10;
+		len--;
+	}
+}
+
 char	*ft_itoa(int n)
 {
 	char	*ret;
 	size_t	len;
-	int		sign;
 
 	len = count_len(n);
+	if (n < 0)
+		len++;
 	ret = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ret)
 		return (NULL);
-	ret[len] = '\0';
-	sign = 1;
-	if (n < 0)
-	{
-		len++;
-		sign = -1;
-		ret[0] = '-';
-	}
-	if (n == 0)
-		ret[0] = '0';
-	while (n)
-	{
-		ret[len - 1] = '0' + sign * (n % 10);
-		n /= 10;
-		len--;
-	}
+	set_nbr_str(ret, n, len);
 	return (ret);
 }
