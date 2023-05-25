4f9a4e9b95647a993db0fdd4b49bec4167f03adf
Susumu Yagi <susumu.yagi@gmail.com>
2023-05-25 10:16:46 +0900

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 16:59:48 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/22 14:52:50 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/05/25 10:15:42 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		start--;
 	}
 	i = 0;
-	while (*(s + i) && i < len)
+	while (s[i] && i < len)
 		i++;
 	ret = (char *)malloc(sizeof(char) * (i + 1));
 	if (!ret)
 		return (NULL);
-	*(ret + i + 1) = '\0';
+	ret[i] = '\0';
 	i = 0;
-	while (*(s + i) && i < len)
+	while (s[i] && i < len)
 	{
-		*(ret + i) = *(s + i);
+		ret[i] = s[i];
 		i++;
 	}
 	return (ret);
