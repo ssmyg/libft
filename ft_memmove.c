8e72974faecd64e5c3b4a4c8b33a543ca2618192
Susumu Yagi <susumu.yagi@gmail.com>
2023-06-01 16:28:49 +0900

    delete test code

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 19:06:26 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/06/01 16:27:38 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/06/01 16:28:27 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,22 +40,3 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	return (dst);
 }
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main()
-{
-	char *dst = malloc(100);
-	char *src = "abc";
-
-printf("%p %p\n",	ft_memmove(dst, "abc", 0),	memmove(dst, "abc", 0));
-printf("%p %p\n",	ft_memmove(dst, NULL, 0),	memmove(dst, NULL, 0));
-printf("%p %p\n",	ft_memmove(NULL, NULL, 5),	memmove(NULL, NULL, 5));
-printf("%p %p\n",	ft_memmove(NULL, NULL, 0),	memmove(NULL, NULL, 0));
-printf("%p %p\n",	ft_memmove(NULL, "src", 0),	memmove(NULL, "src", 0));
-
-ft_memmove(NULL, "src", 0);
-memmove(NULL, "src", 0);
-
-return 0;
-}
