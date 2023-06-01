015500440cf24c3760c6fee9d5467ca5711c34ea
Susumu Yagi <susumu.yagi@gmail.com>
2023-06-01 17:22:24 +0900

    add test code(memmove)

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 19:06:26 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/06/01 16:30:22 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2023/06/01 17:21:59 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
-	if ((len > 0 && !dst && !src))
+	if (!dst && !src)
 		return (NULL);
 	if (dst < src)
 	{
@@ -38,4 +38,28 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		}
 	}
 	return (dst);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	main(void)
+{
+	char	*dst;
+	char	*src;
+
+	dst = malloc(100);
+	src = "abc";
+	printf("%p %p\n", ft_memmove(dst, "abc", 0), memmove(dst, "abc", 0));
+	printf("%p %p\n", ft_memmove(dst, NULL, 0), memmove(dst, NULL, 0));
+	//printf("%p %p\n", ft_memmove(dst, NULL, 5), memmove(dst, NULL, 5));
+	printf("%p %p\n", ft_memmove(NULL, NULL, 5), memmove(NULL, NULL, 5));
+	printf("%p %p\n", ft_memmove(NULL, NULL, 0), memmove(NULL, NULL, 0));
+	printf("%p %p\n", ft_memmove(NULL, "src", 0), memmove(NULL, "src", 0));
+	ft_memmove(NULL, "src", 0);
+	memmove(NULL, "src", 0);
+	//ft_memmove(dst, NULL, 5);
+	//memmove(dst, NULL, 5);
+	return (0);
 }
